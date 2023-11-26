5fe030db8c577145077cd61332be7839012132c0
David Heidari <davidheidari2@gmail.com>
2023-11-26 12:50:08 +0100

    just added some coments to remind myself what i need to do

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ async function getJSON() {
     if (response.ok) {
         const json = await response.json();
         getJSON(json);
+        //console.log(`this should work ${json}`)
         
         
     } else {
@@ -14,73 +15,12 @@ async function getJSON() {
 }
 
 
-function mainSection() {
-    // my mainSection
-    const mainSection = document.createElement('section');
-
-    // my mainSection three child elements
-    const header = document.createElement('header');
-    const main = document.createElement('main');
-    const footer = document.createElement('footer');
-
-    // appending my elements mainSection and childs
-    document.body.appendChild(mainSection);
-    mainSection.appendChild(header);
-    mainSection.appendChild(main);
-    mainSection.appendChild(footer);
-
-    // my header child elements
-    const content = document.createElement('div');
-    const image = document.createElement('div');
-
-    // giving my div-elements inside header each className
-    content.classList.add('content')
-    image.classList.add('image');
-
-    // appendin my header childElements    
-    header.appendChild(content);
-    header.appendChild(image);
-
-    // my main child elements
-    const mainContent1 = document.createElement('div');
-    const mainContent2 = document.createElement('div');
-    const mainContent3 = document.createElement('div');
-
-    // giving my main div-element same className
-    mainContent1.classList.add('mainContent')
-    mainContent2.classList.add('mainContent')
-    mainContent3.classList.add('mainContent')
-
-    // appending my main child divs with same class
-    main.appendChild(mainContent1);
-    main.appendChild(mainContent2);
-    main.appendChild(mainContent3);
-
-    // my footer child-elements
-    const footerContent = document.createElement('div');
-
-    // giving footer child-element a className
-    footerContent.classList.add('footerContent');
-
-    // appending my footer child-element
-    footer.appendChild(footerContent)
-}
-
-mainSection();
-
-
-
-
-
-function myBody() {
-    
-    
-   
-
-    
-    
-}
-myBody();
+// create a headerDiv
+// in header you need at least 2 div with different className 
+// create a mainDiv
+// in mainDiv you need at least 3 div with diffeerent className maybe??
+// create a footerDiv
+// one divChildElement to our footer
 
 
 getJSON();
